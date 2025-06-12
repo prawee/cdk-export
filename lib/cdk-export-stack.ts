bf8107ab9e18341f357583549892106ae8041b74
Prawee Wongsa <prawee@hotmail.com>
2025-06-12 19:55:13 +0700

    create lambda fn

--- lib/cdk-export-stack.ts
+++ lib/cdk-export-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
 
 export class CdkExportStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new NodejsFunction(this, 'CdkExportLambda', {
+      entry: path.join(__dirname, '../lambda/index.ts'),
+      handler: 'handler',
+      runtime: lambda.Runtime.NODEJS_22_X,
+      bundling: {
+        nodeModules: ['hono'],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkExportQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fnUrl = fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new cdk.CfnOutput(this, 'CdkExportLambdaFunctionUrl', {
+      value: fnUrl.url,
+    });
   }
 }
