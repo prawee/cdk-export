771865579ff831932bcdb70464b06756794c3b2b
Prawee Wongsa <prawee@hotmail.com>
2025-06-12 20:29:28 +0700

    fix response #3

--- lambda/index.ts
+++ lambda/index.ts
@@ -14,17 +14,27 @@ app.get('/excel', async (c) => {
     worksheet.addRow([new Date(), 'Data 3'])
 
     const buffer = await workbook.xlsx.writeBuffer()
+    const base64 = Buffer.from(buffer).toString('base64')
 
     // c.header('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     // c.header('Content-Disposition', 'attachment; filename="demo.xlsx"')
     // return c.body(csv)
-    return new Response(buffer, {
-        status: 200,
+    // return new Response(buffer, {
+    //     status: 200,
+    //     headers: {
+    //         'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    //         'Content-Disposition': 'attachment; filename="cdk-export-excel-demo.xlsx"',
+    //     },
+    // })
+    return c.json({
+        isBase64Encoded: true,
+        statusCode: 200,
         headers: {
             'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
             'Content-Disposition': 'attachment; filename="cdk-export-excel-demo.xlsx"',
         },
-    })
+        body: base64,
+    }, 200)
 })
 
 export const handler = handle(app)
