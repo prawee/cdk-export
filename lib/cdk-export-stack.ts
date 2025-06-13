26ac89874773ace75948fdf812917b2e3c016c63
Prawee Wongsa <prawee@hotmail.com>
2025-06-13 09:30:07 +0700

    config lambda with s3 env

--- lib/cdk-export-stack.ts
+++ lib/cdk-export-stack.ts
@@ -15,6 +15,12 @@ export class CdkExportStack extends cdk.Stack {
       bundling: {
         nodeModules: ['hono', 'exceljs'],
       },
+      environment: {
+        S3_REGION: process.env.S3_REGION || 'ap-southeast-1',
+        S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID || '',
+        S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY || '',
+        S3_BUCKET_NAME: process.env.S3_BUCKET_NAME || '',
+      },
     });
 
     const fnUrl = fn.addFunctionUrl({
