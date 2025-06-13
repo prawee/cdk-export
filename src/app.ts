de9e4876a4577df7d65a437ddc98520aa06017b8
Prawee Wongsa <prawee@hotmail.com>
2025-06-13 11:43:56 +0700

    using new response

--- src/app.ts
+++ src/app.ts
@@ -68,17 +68,17 @@ app.get('/excel', async (c) => {
     // return c.json({ url: signedUrl })
     // return c.body(signedUrl)
     console.log('signedUrl', signedUrl)
-    return c.redirect(signedUrl, 302)
+    // return c.redirect(signedUrl, 302)
 
-    // const result = await fetch(signedUrl)
-    // console.log('result', result)
-    // return new Response(result.body, {
-    //     status: 200,
-    //     headers: {
-    //         'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    //         'Content-Disposition': 'attachment; filename="cdk-export-excel-demo.xlsx"',
-    //     },
-    // })
+    const result = await fetch(signedUrl)
+    console.log('result', result)
+    return new Response(result.body, {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'Content-Disposition': 'attachment; filename="cdk-export-excel-demo.xlsx"',
+        },
+    })
 })
 
 export default app
